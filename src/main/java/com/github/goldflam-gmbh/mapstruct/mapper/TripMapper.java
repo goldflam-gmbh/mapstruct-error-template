9c432234ff999f63feb07d3a97b4d14268e60f52
Tobias Fischer <t.fischer@goldflam.de>
2023-01-19 10:31:28 +0100

    initial commit

---
+++ src/main/java/com/github/goldflam-gmbh/mapstruct/mapper/TripMapper.java
@@ -0,0 +1,20 @@
+package org.example.mapstruct.mapper;
+
+import org.example.mapstruct.dto.TripDto;
+import org.example.mapstruct.model.Trip;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.*;
+
+
+@Mapper(componentModel = "spring", uses = {MapperConfig.class, CarMapper.class, PlaneMapper.class, ShipMapper.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+public abstract class TripMapper {
+
+    public abstract TripDto tripToTripDto(Trip trip);
+
+    public abstract Trip tripDtoToTrip(TripDto tripDto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    public abstract Trip updateTripWithTripDto(TripDto update, @MappingTarget Trip destination);
+
+}
